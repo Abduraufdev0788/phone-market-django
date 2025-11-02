d9d7b97407b6f59140f84e6f5f537bd6cff5044e
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-11-02 23:00:45 +0500

    html toliq

--- phone_market/views.py
+++ phone_market/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+def home(requests:HttpRequest):
+    return render(request=requests, template_name="index.html")
